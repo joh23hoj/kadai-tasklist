ee6d3383bcacbcbe8305b1490784c034b103a28c
J <sjoh1204@outlook.jp>
2020-10-22 12:07:40 +0000

    kadai caramutuika

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -8,11 +8,6 @@ use App\Task;
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         // タスク一覧を取得
@@ -24,31 +19,27 @@ class TasksController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $task = new Task;
         
         // タスク作成ビューを表示
-        return view("tasks.create", [
-            "task" => $task,
+        return view('tasks.create', [
+            'task' => $task,
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            "status" => "required|max:10",
+            "content" => "required|max:255",
+        ]);
+        
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -56,52 +47,39 @@ class TasksController extends Controller
         return redirect('/');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         
         // タスク詳細ビューでそれを表示
-        return view("tasks.show", [
-            "task" => $task,
+        return view('tasks.show', [
+            'task' => $task,
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         
         // タスク編集ビューでそれを表示
-        return view("tasks.edit", [
-            "task" => $task,
+        return view('tasks.edit', [
+            'task' => $task,
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            "status" => "required|max:10",
+            "content" => "required|max:255",
+        ]);
        // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -109,12 +87,6 @@ class TasksController extends Controller
         return redirect('/');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //idの値でタスクを検索して取得
